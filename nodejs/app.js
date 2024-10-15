8485e36b5ee7b3b33b2f4828ff9e34caa65ef7e1
Rahmat Hidayat <rahmat6hidayat@gmail.com>
2024-10-16 03:07:12 +0700

    Deskripsi perubahan

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,28 +1,20 @@
-const express = require('express');
 const { Pool } = require('pg');
-const app = express();
 
-// Koneksi ke PostgreSQL
 const pool = new Pool({
-  user: 'postgres', // Ganti dengan username PostgreSQL Anda
-  host: 'localhost', // Jika server lokal
-  database: 'fgmi_database', // Nama database yang telah Anda buat
-  password: '1rupiah_1rupiah_', // Ganti dengan password PostgreSQL Anda
-  port: 5432, // Port default PostgreSQL
+  user: 'postgres',
+  host: 'localhost',
+  database: 'fgmi_database',
+  password: '1rupiah_1rupiah_',
+  port: 5432,
+  ssl: false,  // Nonaktifkan SSL untuk koneksi lokal
 });
 
-// Route untuk mengambil data dari tabel 'cards'
-app.get('/cards', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM cards');
-    res.json(result.rows); // Kirim data dalam format JSON
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
+// Penggunaan pool
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Error executing query', err.stack);
+  } else {
+    console.log('Query result:', res.rows);
   }
-});
-
-// Menjalankan server di port 5000
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+  pool.end();
 });
