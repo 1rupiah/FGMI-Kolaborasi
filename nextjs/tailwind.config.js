5323297480d10b9676be6cb4ac0a914c8db5e7d4
Rahmat Hidayat <rahmat6hidayat@gmail.com>
2024-10-15 23:46:47 +0700

    Fixed line endings issue

--- nextjs/tailwind.config.js
+++ nextjs/tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx}', // Ini untuk Next.js dengan struktur src
-    './pages/**/*.{js,ts,jsx,tsx}', // Jika Anda masih memiliki folder pages di luar src
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}', // Jika Anda menggunakan App Router di Next.js
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
